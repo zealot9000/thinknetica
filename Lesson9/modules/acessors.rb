a46656d52e0d290a46ffa70f8aebc238c5d73fe7
zealot9000 <zilatov@gmail.com>
2017-01-10 22:57:19 +0300

    Lesson9 home work 1.2

--- Lesson9/modules/acessors.rb
+++ Lesson9/modules/acessors.rb
@@ -1,22 +1,40 @@
-module Acessors
-   
-variable_history = []
+module Accessors   
    
   def attr_accessor_with_history(*attributes)
     attributes.each do |attribute|
-      eval %{
-        def #{attribute}
-          @#{attribute}
-        end 
-        
-        def #{attribute}=(nil)
-          @#{attribute} = nil
-          variable_history.push(@#{attribute})
-        end
-      }
-    end  
-  end   
+      var_name = "@#{attribute}".to_sym
+      define_method(attribute) { instance_variable_get(var_name) }
+      
+      define_method("#{attribute}=".to_sym) do |value|
+        instance_variable_set(var_name, value)  
+        @history_array ||= {}
+        @history_array[var_name] ||= []
+        @history_array[var_name] << value
+      end  
+
+      define_method("#{attribute}_history") {@history_array[var_name]}
+    end
+  end     
   
-  def strong_attr_acessor
+  def strong_attr_accessor(attribute, type)
+    var_name = "@#{attribute}".to_sym
+    define_method(attribute) { instance_variable_get(var_name) }
+
+    define_method("#{attribute}=".to_sym) do |value|
+      if value.class == type
+        instance_variable_set(var_name, value)
+      else
+        raise "Invalid attribute class."
+      end  
+    end          
   end  
+end
+
+
+
+class Test
+  extend Accessors
+
+  attr_accessor_with_history :my_attr, :a, :b
+
 end
